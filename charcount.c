f9f79a7d0d5e51c8b704e12f2b186834fe75d476
daviddkkim <david.kim@richmond.edu>
2023-06-13 23:31:37 -0700

    charcount

---
+++ charcount.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* copy input to output; 1st version  */
+
+int main() {
+
+  long count;
+  int c;
+  count = 0;
+  c = getchar();
+  while (c != EOF) {
+    ++count;
+    c = getchar();
+    printf("%ld\n", count);
+  }
+}
