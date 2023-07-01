9c399a99412547083b2d0984ae93d8c57a61dbe2
daviddkkim <david.kim@richmond.edu>
2023-07-01 18:38:04 -0400

    extern

---
+++ 4-extern.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int x = 5;
+
+void incAndPrint() {
+  x++;
+  printf("%d", x);
+}
---
+++ 4.c
@@ -0,0 +1,9 @@
+#include "4-extern.c"
+#include <stdio.h>
+
+int x;
+
+int main() {
+  incAndPrint();
+  printf("%d", x);
+}
